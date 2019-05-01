b9da5d33bd4d3f5f51821226d74fca88eb067d59
xin hu <xinhu@xins-MBP.fios-router.home>
2019-04-30 20:25:06 -0400

    feature(package):set up basic package json

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          feather-knowledge
         </a>
       </header>
     </div>
