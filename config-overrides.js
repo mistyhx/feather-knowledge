d499deb91e1df326ebc6ef3b309441270ea03794
xin hu <xinhu@xins-MBP.fios-router.home>
2019-05-05 18:47:36 -0400

    update(globalstyle):changed global primary color and background color

--- config-overrides.js
+++ config-overrides.js
@@ -1,12 +1,13 @@
 const { override, fixBabelImports, addLessLoader } = require("customize-cra");
 
 module.exports = override(
-    fixBabelImports("import", {
-        libraryName: "antd",
-        libraryDirectory: "es",
-        style: true, // change importing css to less
-    }),
-    addLessLoader({
-        javascriptEnabled: true,
-    })
+  fixBabelImports("import", {
+    libraryName: "antd",
+    libraryDirectory: "es",
+    style: true, // change importing css to less
+  }),
+  addLessLoader({
+    javascriptEnabled: true,
+    modifyVars: { "@primary-color": "#205FE4", "@background-color-base": "#E7EEF8" },
+  })
 );
