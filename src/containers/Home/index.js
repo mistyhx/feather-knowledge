c3446f935ea8ff54f9eae2856be2de5b976d2444
xin hu <xinhu@xins-MBP.fios-router.home>
2019-05-05 10:55:59 -0400

    feature(home):added topic cards

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Card, List } from "antd";
+import "./index.less";
 
 const data = [
   {
@@ -18,20 +19,41 @@ const data = [
 
 function Home() {
   return (
-    <Card>
-      <List
-        itemLayout="horizontal"
-        dataSource={data}
-        renderItem={item => (
-          <List.Item>
-            <List.Item.Meta
-              title={<a href="https://ant.design">{item.title}</a>}
-              description="Ant Design, a design language for background applications, is refined by Ant UED Team"
-            />
-          </List.Item>
-        )}
-      />
-    </Card>
+    <div>
+      <Card>
+        <List
+          itemLayout="horizontal"
+          dataSource={data}
+          renderItem={item => (
+            <List.Item>
+              <List.Item.Meta
+                title={<a href="https://ant.design">{item.title}</a>}
+                description="Ant Design, a design language for background applications, is refined by Ant UED Team"
+              />
+            </List.Item>
+          )}
+        />
+      </Card>
+      <div className="topics">
+        <List
+          grid={{
+            gutter: 16,
+            xs: 1,
+            sm: 2,
+            md: 4,
+            lg: 4,
+            xl: 6,
+            xxl: 3,
+          }}
+          dataSource={data}
+          renderItem={item => (
+            <List.Item>
+              <Card title={item.title}>Card content</Card>
+            </List.Item>
+          )}
+        />
+      </div>
+    </div>
   );
 }
 
