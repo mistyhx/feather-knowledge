ef080193dc1aa31eb3ec762e55e31800dcd3236d
xin hu <xinhu@xins-MBP.fios-router.home>
2019-05-19 19:42:02 -0400

    added-logo

--- src/components/GlobalSider/index.js
+++ src/components/GlobalSider/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Layout, Menu, Icon } from "antd";
+import featherLogo from "../../assets/feather_logo.svg";
 import "./index.less";
 
 const { Sider } = Layout;
@@ -7,6 +8,9 @@ const { Sider } = Layout;
 function GlobalSider() {
   return (
     <Sider className="sider">
+      <div className="logo">
+        <img src={featherLogo} alt="feather-logo" width="60px" />
+      </div>
       <Menu className="sider-menu" theme="light" defaultSelectedKeys={["1"]}>
         <Menu.Item key="1">
           <Icon type="home" />
